60f4a6e00ffc514ffc42f9daacf1ea7fdd8a5d78
fahadsaleem164 <igz.iwd.25@gmail.com>
2021-02-02 15:48:18 +0500

    first commit

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f8153ce992e49b9ea962d612690dbbbf'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '519e3c58-4edd-48c9-a68c-7601f5df48ee'
+};
